9402ff04c0ed3f1d5b9d327098bcc535d87018b7
akitazu14 <greatarcher14@yahoo.co.jp>
2020-09-12 10:35:07 +0900

    add association to user-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :perchases
   validates :nickname, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birth_year_id, :birth_month_id, :birth_day_id ,presence: true
   validates :family_name_kanji, :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :family_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/}
