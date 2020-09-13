9c8afac4f254be388e7f60061bb50e49bb550302
akitazu14 <greatarcher14@yahoo.co.jp>
2020-09-13 21:52:09 +0900

    create sends-model & prefecture

--- app/controllers/sends_controller.rb
+++ app/controllers/sends_controller.rb
@@ -1,2 +1,31 @@
 class SendsController < ApplicationController
+
+  def new
+    @send = Send.new
+  end
+
+  def create
+    @send = Send.new(send_params)
+    if @send.save
+      redirect_to root_path, notice: '送付先情報を登録しました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def send_params
+    params.require(:send).permit(
+      :family_name_kanji, 
+      :first_name_kanji, 
+      :family_name_kana, 
+      :first_name_kana, 
+      :postcode, 
+      :prefecture_id, 
+      :city, 
+      :block, 
+      :building, 
+      :tel
+    ).merge(user_id: current_user.id)
+  end
 end
