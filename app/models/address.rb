7b807c004b873793c43e45ee2a450db047439afb
akitazu14 <greatarcher14@yahoo.co.jp>
2020-09-15 22:56:51 +0900

    create address model

---
+++ app/models/address.rb
@@ -0,0 +1,15 @@
+class Address < ApplicationRecord
+  belongs_to :user, optional: true
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  validates :prefecture, presence: true
+
+  with_options numericality: { other_than: 0 } do
+    validates :prefecture_id
+  end
+
+  validates :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :postcode, :city, :block , presence: true
+  validates :family_name_kanji, :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/}
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_052024) do
+ActiveRecord::Schema.define(version: 2020_09_15_132219) do
 
-  create_table "sends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name_kanji", null: false
     t.string "first_name_kanji", null: false
     t.string "family_name_kana", null: false
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_09_13_052024) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_sends_on_user_id"
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -47,5 +47,5 @@ ActiveRecord::Schema.define(version: 2020_09_13_052024) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "sends", "users"
+  add_foreign_key "addresses", "users"
 end
