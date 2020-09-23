8072dc9eeb5e12918affe230dde894deb50a2151
shunAutumn <tarosan.programming@gmail.com>
2020-09-23 16:39:56 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :address
+  has_many :items
+
   validates :nickname, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birthday, presence: true
   validates :family_name_kanji, :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :family_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/}
