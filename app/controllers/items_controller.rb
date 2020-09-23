52148e5594c4b9d68220c0d63f0d4fca69e9b2c2
shunAutumn <tarosan.programming@gmail.com>
2020-09-23 16:38:57 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,31 +1,26 @@
 class ItemsController < ApplicationController
-  def index
-  end
-  
-  def show
+  before_action :authenticate_user!, only: [:new, :create]
 
-  end
-  
   def new
-
-  end
-
-  def edit
-
+    flash[:notice] = "ログイン済みユーザーのみ出品できます" unless user_signed_in?
+    @item = Item.new
+    @item.item_images.new
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, alert: "出品できませんでした"
+    end
   end
-
-  def update
-
-  end
-
-  def destroy
-
-  end
-
+  
   private
   
+  def item_params
+    params.require(:item).permit(
+      :product_name, :description, :category_id, :brand_id, :condition_id, :shipping_fee_id, :prefecture_id, :day_to_ship_id, :price, item_images_attributes: [:src]
+    ).merge(user_id: current_user.id)
+  end
 end
