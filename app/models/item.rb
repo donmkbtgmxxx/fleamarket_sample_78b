512fd63c536a871b9c57cbf96ec046dd23b3b561
shunAutumn <tarosan.programming@gmail.com>
2020-10-02 12:12:02 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
-has_many :item_images
-accepts_nested_attributes_for :item_images, allow_destroy: true
-belongs_to :user
-belongs_to :brand, optional: true
+  has_many :item_images, dependent: :destroy
+  accepts_nested_attributes_for :item_images, allow_destroy: true
+  belongs_to :user
+  belongs_to :brand, optional: true
+  has_one :purchase
+  belongs_to :category
 
 extend ActiveHash::Associations::ActiveRecordExtensions
-belongs_to_active_hash :category
 belongs_to_active_hash :condition
 belongs_to_active_hash :ShippingFee
 belongs_to_active_hash :DayToShip
@@ -22,4 +23,6 @@ belongs_to_active_hash :prefecture
     validates :price
     validates :item_image_ids
   end
+
+  validates :price, format: {with: /\A[0-9０-９]+\z/}
 end
