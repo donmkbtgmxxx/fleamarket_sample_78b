c9deac034505f6758022630ee3929a27dc1cde9c
shunAutumn <tarosan.programming@gmail.com>
2020-10-02 12:12:08 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one :address
   has_many :items
+  has_many :purchases
 
   validates :nickname, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birthday, presence: true
   validates :family_name_kanji, :first_name_kanji, format: { with: /\A[ぁ-んァ-ン一-龥]/}
