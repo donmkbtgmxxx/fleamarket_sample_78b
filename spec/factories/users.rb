a14eeb2138e27f5dcf966f29eec776b6e7007767
shunAutumn <tarosan.programming@gmail.com>
2020-10-11 00:13:17 +0900

    データをfakerで自動生成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
+    password = Faker::Internet.password(7)
+    nickname              { Faker::Name.last_name }
+    email                 { Faker::Internet.free_email }
+    password              { password }
+    password_confirmation { password }
     family_name_kanji     {"山田"}
     first_name_kanji      {"太郎"}
     family_name_kana      {"やまだ"}
